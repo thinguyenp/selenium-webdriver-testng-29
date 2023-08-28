96e07342fbf6edd72a166a1612eedf722fe05000
thinguyenp <137849173+thinguyenp@users.noreply.github.com>
2023-08-28 22:36:43 +0700

    adding new scripts

---
+++ selenium/webdriver/Topic_03_RelativeLocators.java
@@ -0,0 +1,45 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_03_RelativeLocators {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_01()
+    {
+
+    }
+
+    @Test
+    public void TC_02()
+    {
+
+
+    }
+    @AfterTest
+    public void afterClass() {
+        driver.quit();
+    }
+}
---
+++ selenium/webdriver/Topic_04_Xpath.java
@@ -0,0 +1,47 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_04_Xpath {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://live.techpanda.org/index/php");
+    }
+
+    @Test
+    public void TC_01()
+    {
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']"));
+
+
+    }
+
+    @Test
+    public void TC_02()
+    {
+
+    }
+    @AfterTest
+    public void afterClass() {
+        driver.quit();
+    }
+}
